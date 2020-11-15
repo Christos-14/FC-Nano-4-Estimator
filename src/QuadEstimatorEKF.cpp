11d54ef554aee7829141f2ef03658f51a0be8fa7
Christos <christos.pantso@gmail.com>
2020-11-15 13:46:56 +0000

    Steps 1 and 2 PASS

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,13 +93,24 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  //float predictedPitch = pitchEst + dtIMU * gyro.y;
+  //float predictedRoll = rollEst + dtIMU * gyro.x;
+  //ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
 
-  // normalize yaw to -pi .. pi
-  if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
-  if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+  //// normalize yaw to -pi .. pi
+  //if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
+  //if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+
+  // We use the state to define a quaternion qt, for the Euler angles phi, theta and psi.
+  Quaternion<float> qt = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+
+  // Integration using Quaternion class method.
+  qt.IntegrateBodyRate(gyro, dtIMU);
+
+  // Predicted pitch and roll and yaw, utilising Quaternion class methods.
+  float predictedPitch = qt.Pitch();
+  float predictedRoll = qt.Roll();
+  ekfState(6) = qt.Yaw();
 
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
